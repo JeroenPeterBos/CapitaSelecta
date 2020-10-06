70cc74427848a012f091dc65c357a98e9ddfd7b6
Jeroen Bos <jeroenbosleusden@gmail.com>
2020-10-06 21:58:42 +0200

    Multi with conv tail

--- src/main/python/dmv/dense.py
+++ src/main/python/dmv/dense.py
@@ -1,3 +1,25 @@
+# This is a copy straight modified from tensorflow.keras.applications.densenet
+
+# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+# pylint: disable=invalid-name
+"""DenseNet models for Keras.
+Reference paper:
+  - [Densely Connected Convolutional Networks]
+    (https://arxiv.org/abs/1608.06993) (CVPR 2017 Best Paper Award)
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -33,6 +55,72 @@ DENSENET201_WEIGHT_PATH_NO_TOP = (
 layers = VersionAwareLayers()
 
 
+def dense_block(x, blocks, name):
+    """A dense block.
+    Arguments:
+      x: input tensor.
+      blocks: integer, the number of building blocks.
+      name: string, block label.
+    Returns:
+      Output tensor for the block.
+    """
+    for i in range(blocks):
+        x = conv_block(x, 32, name=name + '_block' + str(i + 1))
+    return x
+
+
+def transition_block(x, reduction, name):
+    """A transition block.
+    Arguments:
+      x: input tensor.
+      reduction: float, compression rate at transition layers.
+      name: string, block label.
+    Returns:
+      output tensor for the block.
+    """
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name=name + '_bn')(
+            x)
+    x = layers.Activation('relu', name=name + '_relu')(x)
+    x = layers.Conv2D(
+        int(backend.int_shape(x)[bn_axis] * reduction),
+        1,
+        use_bias=False,
+        name=name + '_conv')(
+            x)
+    x = layers.AveragePooling2D(2, strides=2, name=name + '_pool')(x)
+    return x
+
+
+def conv_block(x, growth_rate, name):
+    """A building block for a dense block.
+    Arguments:
+      x: input tensor.
+      growth_rate: float, growth rate at dense layers.
+      name: string, block label.
+    Returns:
+      Output tensor for the block.
+    """
+    bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    x1 = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name=name + '_0_bn')(
+            x)
+    x1 = layers.Activation('relu', name=name + '_0_relu')(x1)
+    x1 = layers.Conv2D(
+        4 * growth_rate, 1, use_bias=False, name=name + '_1_conv')(
+            x1)
+    x1 = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name=name + '_1_bn')(
+            x1)
+    x1 = layers.Activation('relu', name=name + '_1_relu')(x1)
+    x1 = layers.Conv2D(
+        growth_rate, 3, padding='same', use_bias=False, name=name + '_2_conv')(
+            x1)
+    x = layers.Concatenate(axis=bn_axis, name=name + '_concat')([x, x1])
+    return x
+
+
 def DenseNet(
         blocks,
         include_top=True,
@@ -41,7 +129,8 @@ def DenseNet(
         input_shape=None,
         pooling=None,
         classes=1000,
-        classifier_activation='softmax'):
+        classifier_activation='softmax',
+        split=False):
     """Instantiates the DenseNet architecture.
     Reference:
     - [Densely Connected Convolutional Networks](
@@ -138,7 +227,7 @@ def DenseNet(
     x = dense_block(x, blocks[2], name='conv4')
     x = transition_block(x, 0.5, name='pool4')
 
-    intermediate_out = x
+    intermediate = x
 
     x = dense_block(x, blocks[3], name='conv5')
 
@@ -218,9 +307,11 @@ def DenseNet(
     elif weights is not None:
         model.load_weights(weights)
 
-    return training.Model(inputs, intermediate_out, name='tailless_densenet')
+    return model, inputs, intermediate, x
 
 
+@keras_export('keras.applications.densenet.DenseNet121',
+              'keras.applications.DenseNet121')
 def DenseNet121(include_top=True,
                 weights='imagenet',
                 input_tensor=None,
@@ -230,3 +321,91 @@ def DenseNet121(include_top=True,
     """Instantiates the Densenet121 architecture."""
     return DenseNet([6, 12, 24, 16], include_top, weights, input_tensor,
                     input_shape, pooling, classes)
+
+
+@keras_export('keras.applications.densenet.DenseNet169',
+              'keras.applications.DenseNet169')
+def DenseNet169(include_top=True,
+                weights='imagenet',
+                input_tensor=None,
+                input_shape=None,
+                pooling=None,
+                classes=1000):
+    """Instantiates the Densenet169 architecture."""
+    return DenseNet([6, 12, 32, 32], include_top, weights, input_tensor,
+                    input_shape, pooling, classes)
+
+
+@keras_export('keras.applications.densenet.DenseNet201',
+              'keras.applications.DenseNet201')
+def DenseNet201(include_top=True,
+                weights='imagenet',
+                input_tensor=None,
+                input_shape=None,
+                pooling=None,
+                classes=1000):
+    """Instantiates the Densenet201 architecture."""
+    return DenseNet([6, 12, 48, 32], include_top, weights, input_tensor,
+                    input_shape, pooling, classes)
+
+
+@keras_export('keras.applications.densenet.preprocess_input')
+def preprocess_input(x, data_format=None):
+    return imagenet_utils.preprocess_input(
+        x, data_format=data_format, mode='torch')
+
+
+@keras_export('keras.applications.densenet.decode_predictions')
+def decode_predictions(preds, top=5):
+    return imagenet_utils.decode_predictions(preds, top=top)
+
+
+preprocess_input.__doc__ = imagenet_utils.PREPROCESS_INPUT_DOC.format(
+    mode='',
+    ret=imagenet_utils.PREPROCESS_INPUT_RET_DOC_TORCH,
+    error=imagenet_utils.PREPROCESS_INPUT_ERROR_DOC)
+decode_predictions.__doc__ = imagenet_utils.decode_predictions.__doc__
+
+DOC = """
+  Reference paper:
+  - [Densely Connected Convolutional Networks]
+    (https://arxiv.org/abs/1608.06993) (CVPR 2017 Best Paper Award)
+  Optionally loads weights pre-trained on ImageNet.
+  Note that the data format convention used by the model is
+  the one specified in your Keras config at `~/.keras/keras.json`.
+  Arguments:
+    include_top: whether to include the fully-connected
+      layer at the top of the network.
+    weights: one of `None` (random initialization),
+      'imagenet' (pre-training on ImageNet),
+      or the path to the weights file to be loaded.
+    input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+      to use as image input for the model.
+    input_shape: optional shape tuple, only to be specified
+      if `include_top` is False (otherwise the input shape
+      has to be `(224, 224, 3)` (with `'channels_last'` data format)
+      or `(3, 224, 224)` (with `'channels_first'` data format).
+      It should have exactly 3 inputs channels,
+      and width and height should be no smaller than 32.
+      E.g. `(200, 200, 3)` would be one valid value.
+    pooling: Optional pooling mode for feature extraction
+      when `include_top` is `False`.
+      - `None` means that the output of the model will be
+          the 4D tensor output of the
+          last convolutional block.
+      - `avg` means that global average pooling
+          will be applied to the output of the
+          last convolutional block, and thus
+          the output of the model will be a 2D tensor.
+      - `max` means that global max pooling will
+          be applied.
+    classes: optional number of classes to classify images
+      into, only to be specified if `include_top` is True, and
+      if no `weights` argument is specified.
+  Returns:
+    A Keras model instance.
+"""
+
+setattr(DenseNet121, '__doc__', DenseNet121.__doc__ + DOC)
+setattr(DenseNet169, '__doc__', DenseNet169.__doc__ + DOC)
+setattr(DenseNet201, '__doc__', DenseNet201.__doc__ + DOC)
--- src/main/python/dmv/layer.py
+++ src/main/python/dmv/layer.py
@@ -1,5 +1,7 @@
 from tensorflow.keras.layers import AbstractRNNCell, RNN
+from tensorflow.python.keras.layers.convolutional_recurrent import ConvRNN2D
 import tensorflow.keras.backend as K
+
 
 class DynamicMultiViewRNNCell(AbstractRNNCell):
     def build(self, input_shape):
@@ -13,10 +15,40 @@ class DynamicMultiViewRNNCell(AbstractRNNCell):
     def call(self, inputs, states):
         output = K.maximum(inputs, states[0])
         return output, output
-    
+
+
 class DynamicMultiViewRNN(RNN):
     def __init__(self, **kwargs):
         cell = DynamicMultiViewRNNCell(**kwargs)
+        super().__init__(cell)
+    
+    def call(self, inputs):
+        return super().call(inputs)
+
+
+class DynamicMultiViewRNNCell2D(AbstractRNNCell):
+    def __init__(self,
+                 data_format='channels_last',
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.data_format = data_format
+
+    def build(self, input_shape):
+        self.units = input_shape[-1]
+        self.built = True
+        
+    @property
+    def state_size(self):
+        return self.units
+    
+    def call(self, inputs, states):
+        output = K.maximum(inputs, states[0])
+        return output, output
+
+
+class DynamicMultiViewRNN2D(ConvRNN2D):
+    def __init__(self, **kwargs):
+        cell = DynamicMultiViewRNNCell2D(**kwargs)
         super().__init__(cell)
     
     def call(self, inputs):
