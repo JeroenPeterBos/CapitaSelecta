31f3c9abb37effc84748cbacdd8d17e8faa079d4
Jeroen Bos <jeroenbosleusden@gmail.com>
2020-10-27 11:10:21 +0100

    Log some system info

--- src/main/python/dmv/main.py
+++ src/main/python/dmv/main.py
@@ -4,12 +4,31 @@ from pathlib import Path
 from dmv.parser import parse_args
 from dmv.experiment import load_data, experiment
 
-import logging
+import platform, socket, re, uuid, json, psutil, logging, multiprocessing
+
 logger = logging.getLogger()
 
 
+def getSystemInfo():
+    try:
+        info = dict()
+        info['platform'] = platform.system()
+        info['platform-release'] = platform.release()
+        info['platform-version'] = platform.version()
+        info['architecture'] = platform.machine()
+        info['hostname'] = socket.gethostname()
+        info['ip-address'] = socket.gethostbyname(socket.gethostname())
+        info['mac-address'] = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        info['processor'] = platform.processor()
+        info['ram'] = str(round(psutil.virtual_memory().total / (1024.0 ** 3))) + " GB"
+        info['cores'] = multiprocessing.cpu_count()
+        return info
+    except Exception as e:
+        logging.exception(e)
+
+
 def setup_logger(log_dir):
-    log_file: Path = args.logs / f'log_{datetime.now().strftime("%m-%d_%H-%M")}.log'
+    log_file: Path = log_dir / f'log_{datetime.now().strftime("%m-%d_%H-%M")}.log'
     log_file.parent.mkdir(parents=True, exist_ok=True)
 
     logger.setLevel(logging.DEBUG)
@@ -30,10 +49,15 @@ def setup_logger(log_dir):
     logger.addHandler(fh)
 
 
+def describe_environment(args):
+    logger.info(f"The system we are running on: \n\n{getSystemInfo()}")
+    logger.info(f"The parameters describing the run are :\n\n{vars(args)}")
+
+
 def main(args):
     setup_logger(args.logs)
+    describe_environment(args)
     logger.info("Starting a new experiment")
-    logger.info(f"The parameters describing the run are :\n\n{vars(args)}")
 
     train_dc, valid_dc = load_data(
         data_folder=args.data,
